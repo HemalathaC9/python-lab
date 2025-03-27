be3f3d4c1614a327f0b09270f37b797d54864f3e
HemalathaC9 <yelgurihemalatha03@gmail.com>
2025-03-27 11:12:18 +0530

    ranNIF.py

---
+++ week2/ranNIF.py
@@ -0,0 +1,14 @@
+import random
+n=random.randint(1,10)
+while True:
+  x=int(input("Enter your number"))
+  if x<n:
+     print("Too low")
+  elif x>n:
+     print("Too high")
+  elif:x==n:
+     print("you got your number")
+     break
+  
+  
+  
