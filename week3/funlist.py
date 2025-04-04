995d27cb9bb8d6a086ccb6a9fbe5be946228e3e2
HemalathaC9 <yelgurihemalatha03@gmail.com>
2025-04-04 15:54:30 +0530

    Create funlist.py

---
+++ week3/funlist.py
@@ -0,0 +1,8 @@
+def printelement(elements):
+   print(elements)
+elements=[]
+limit=int(input("enter no of list"))
+for i in range(limit):
+  ele=int(input())
+ elements.append(ele)
+printelements(elements)
